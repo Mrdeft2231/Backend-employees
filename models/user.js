6672ca99d68c62803cf748f4bcb8bf0d29b44a37
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-06-07 23:10:48 +0300

    add добавил авторизацию

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  login: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+  .then(user => {
+    if (!user) {
+
+      return Promise.reject(new Error("Неправильная почта или пароль"));
+
+    }
+    return bcrypt.compare( password, user.password ).then(matched => {
+      if (!matched) {
+        return Promise.reject(new Error("Неправильная почта или пароль"));
+      }
+      return user;
+    })
+  
+  })
+};
+
+module.exports = mongoose.model('auth', userSchema);
