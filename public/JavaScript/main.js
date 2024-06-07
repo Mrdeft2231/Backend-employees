1a54fe0e5355a149e480f607b409c37833208bb5
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-06-07 20:50:51 +0300

    add: доработал форму сотрудников

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -2,9 +2,24 @@
 let image = document.querySelector('.photo');
 let file = document.querySelector('#photo');
 
+let employeeButton = document.querySelector('#employee-button');
+let staffEmployee = document.querySelector('.Staff-Editor-item-shadow');
+let buttonExist = document.querySelector('#button-staff-exit');
 
 // Функция для загрузки фото
 file.addEventListener('change', function() {
   image.src = URL.createObjectURL(file.files[0]);
   image.computedStyleMap.display = 'block';
 })
+
+// Кнопки включения и отключения редактора сотрудника
+
+employeeButton.addEventListener('click', function() {
+  staffEmployee.style.display = 'block';
+ })
+
+buttonExist.addEventListener('click', function() {
+  staffEmployee.style.display = 'none';
+ })
+
+ 
