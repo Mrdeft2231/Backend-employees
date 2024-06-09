68308adcb6a902c2b861c25a9b24c6d83838e227
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-06-09 11:16:29 +0300

    add

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   login: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
