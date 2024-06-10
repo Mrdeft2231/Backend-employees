95a64309667fe78b2fda591d36e8b5211c1373fe
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-06-10 22:16:38 +0300

    add: доделал форму пользователей на фронте

--- public/JavaScript/addingEmployees.js
+++ public/JavaScript/addingEmployees.js
@@ -1,14 +1,61 @@
+const userInput = document.getElementById('login-name');
+const passwordInput = document.getElementById('password-user');
 
-function hangleFormSubmit(event) {
+
+
+async function hangleFormSubmit(event) {
   event.preventDefault()
-  serializeForm(applicationForm)
-  console.log('Отправка успешно');
+  const data = serializeForm(event.target)
+
+  toggleLoader();
+  const { status, error } = await sendData(data)
+  toggleLoader();
+
+
+  if (status === 200) {
+    onSuccess(event.target)
+    clearInput();
+  } else {
+    onError(error);
+  }
 }
 
 function serializeForm(formNode) {
-  console.log(Array.from(data.entries()))
-  return new FormData(formNode);
+const data = new FormData(formNode)
+console.log('Данные с формы', Array.from(data.entries()))
+return data;
 }
 
+async function sendData(data) {
+  return await fetch('/api/user/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'multipart/form-data' },
+    body: data,
+  })
+}
+
+function clearInput() {
+  userInput.value = '';
+  passwordInput.value = '';
+}
+
+function toggleLoader() {
+  const loader = document.getElementById('loader')
+  loader.classList.toggle('hidden')
+}
+
+function onSuccess(formNode) {
+  alert('Данные успешно отправленны')
+  formNode.classList.toggle('hidden')
+}
+
+function onError(error) {
+
+  alert(error.message);
+}
+
+
+
 const applicationForm = document.getElementById('user');
-applicationForm.addEventListener('submit', hangleFormSubmit)
+applicationForm.addEventListener('submit', hangleFormSubmit);
+
