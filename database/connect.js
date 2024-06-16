0da37cf0d703bb9ca71504306914d3ca530342bb
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-06-16 23:12:56 +0300

    Исправление

--- database/connect.js
+++ database/connect.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose");
-const { createModel } = require('mongoose-gridfs');
 
 const DB_URL = "mongodb://localhost:27017/admin";
 
 async function connectToDatabase() {
 try {
   await mongoose.connect(DB_URL)
-  const Attachment = createModel({
-    modelName: 'Attachment',
-    connection: connection
-  });
 
-  mongoose.Attachment = Attachment;
   console.log("Успешное подключение к базе");
 } catch (err) {
   console.log("Не удалось подключиться к базе");
